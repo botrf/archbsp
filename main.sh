8f60b50ba678bae1922484f9f976657d31dee975
Your Name <you@example.com>
2021-12-16 12:21:28 +0200

    first commit

---
+++ main.sh
@@ -0,0 +1,14 @@
+sudo pacman -S terminus-font;
+
+setfont ter-v20n;
+
+sudo pacman -S xf86-video-intel xorg xorg-xinit bspwm picom dmenu  chromium feh xfce4-terminal arandr sxhkd thunar lxappearance flameshot ;
+
+cp .config/ ~/;
+cp .xinitrc/ ~/;
+cp .Xresources/ ~/;
+cp .fonts ~/;
+cp .themes ~/;
+cp .icons ~/;
+
+
